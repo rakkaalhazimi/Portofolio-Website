fc2772f78fd43eafa2324eaccd41dffdbed72441
unknown <rakkakeren@gmail.com>
2022-02-05 13:46:23 +0700

    build landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   mode: 'jit',
   purge: ['./app/templates/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#292828",
+        secondary: "#cb6ce6",
+      }
+    }
   },
   variants: {
     extend: {},
