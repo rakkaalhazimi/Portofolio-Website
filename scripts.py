65ebec83c433aa28609a3ad6ef7d8c3f6f526e9c
unknown <rakkakeren@gmail.com>
2022-02-20 18:12:27 +0700

    update .gitignore

--- scripts.py
+++ scripts.py
@@ -20,16 +20,17 @@ def update_data(filter_dict, update_dict):
 
 
 if __name__ == "__main__":
-    add_data(
-        title="Indonesian Minimum Wage Dashboard",
-        url="https://public.tableau.com/app/profile/rakka.alhazimi/viz/shared/6F44MGH8H",
-        image_path="projects/ump_indonesia.png",
-        icon_path="projects/tableau.png",
-        description="""
-        Analysis of Indonesian minimum wage in 2021 through geographic visualization. The data is consist 
-        of 33 provinces of Indonesian and was scrapped from minister of manpower website.
-        """,
-    )
+    ...
+    # add_data(
+    #     title="Indonesian Minimum Wage Dashboard",
+    #     url="https://public.tableau.com/app/profile/rakka.alhazimi/viz/shared/6F44MGH8H",
+    #     image_path="projects/ump_indonesia.png",
+    #     icon_path="projects/tableau.png",
+    #     description="""
+    #     Analysis of Indonesian minimum wage in 2021 through geographic visualization. The data is consist 
+    #     of 33 provinces of Indonesian and was scrapped from minister of manpower website.
+    #     """,
+    # )
 
     # update_data(
     #     filter_dict=...,
