a5e479634fd3647505ea3440f6b5b67d509b0d79
unknown <rakkakeren@gmail.com>
2022-02-20 21:18:21 +0700

    change database models

--- app/models.py
+++ app/models.py
@@ -2,10 +2,10 @@ from . import db
 
 class Projects(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(100), nullable=False)
-    title = db.Column(db.String(50), nullable=False)
-    image_path = db.Column(db.String(30), nullable=False)
-    icon_path = db.Column(db.String(30), nullable=False)
-    description = db.Column(db.String(150), nullable=False)
+    url = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    image_path = db.Column(db.String(255), nullable=False)
+    icon_path = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
 
 db.create_all()
