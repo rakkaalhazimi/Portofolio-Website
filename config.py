bea50d57b7e369fe6407c72f29dbaba96831b6f7
unknown <rakkakeren@gmail.com>
2022-02-21 06:20:06 +0700

    update config

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ class Config(object):
     RESUME_PATH = os.path.join(base_dir, "app/static/download/Resume-PythonDeveloper.pdf")
 
     DATABASE = os.path.join(base_dir, "app/main.db")
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@ec2-54-209-221-231.compute-1.amazonaws.com:5432/dd6qv5s1bur2gf'.format(os.environ["POSGRES_USERNAME"], os.environ["POSGRES_PASSWORD"])
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}'.format(os.environ["POSGRES_USERNAME"], os.environ["POSGRES_PASSWORD"], os.environ["POSGRES_SERVER"])
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'jdlfjds7834kjfksdfhdsds'
     
 
 class ProductionConfig(Config):
-    DATABASE_URI = 'mysql://user@localhost/foo'
+    ...
 
 class DevelopmentConfig(Config):
     DEBUG = True
