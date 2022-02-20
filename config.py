c8c79a7cfd0fd78695436f5de71e209b3cf4c494
unknown <rakkakeren@gmail.com>
2022-02-21 06:07:36 +0700

    change config and update gitignore, .env

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
+base_dir = os.path.dirname(__file__)
 
 class Config(object):
     DEBUG = False
     TESTING = False
 
-    BASE_DIR = os.path.dirname(__file__)
-    RESUME_PATH = os.path.join(BASE_DIR, "app/static/download/Resume-PythonDeveloper.pdf")
+    RESUME_PATH = os.path.join(base_dir, "app/static/download/Resume-PythonDeveloper.pdf")
 
-    DATABASE = os.path.join(BASE_DIR, "app/main.db")
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{DATABASE}'
+    DATABASE = os.path.join(base_dir, "app/main.db")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@ec2-54-209-221-231.compute-1.amazonaws.com:5432/dd6qv5s1bur2gf'.format(os.environ["POSGRES_USERNAME"], os.environ["POSGRES_PASSWORD"])
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'jdlfjds7834kjfksdfhdsds'
