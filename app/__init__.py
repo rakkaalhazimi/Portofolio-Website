7b3802032940670facd2d754e320f2729ae48cda
unknown <rakkakeren@gmail.com>
2022-02-21 06:32:28 +0700

    add migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Import configuration profile based on FLASK_ENV variable - defaults to Production
 if os.environ.get('FLASK_ENV') == 'development':
